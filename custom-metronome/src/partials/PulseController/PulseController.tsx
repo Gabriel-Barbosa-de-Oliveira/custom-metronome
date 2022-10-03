a2092f04d743bbc4db8192be6af40b1eae1a345f
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-10-03 17:07:32 -0300

    Iniciando estado de controle

--- custom-metronome/src/partials/PulseController/PulseController.tsx
+++ custom-metronome/src/partials/PulseController/PulseController.tsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import { IPulseController } from '../../shared/interfaces/IPulseController';
 import "./PulseController.scss";
-export default class PulseController extends Component {
+export default class PulseController extends Component<{}, IPulseController> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            beatsNumber: 4 
+        };
+    }
     render() {
+        let dots: Array<JSX.Element> = [];
+
+        for (let index = 0; index < this.state.beatsNumber; index++) {
+            dots.push(<span className="dot"></span>);
+        }
+
         return (
             <div className='pulse-control-container'>
-                <span className="dot"></span>
-                <span className="dot active"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
+                {dots}
             </div>
         )
     }
