74edf6d694a0590c50f0c8ea36f840d5518614b0
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-10-06 17:28:28 -0300

    Corre;óes de estilização landing page

--- custom-metronome/src/components/Landing/Landing.tsx
+++ custom-metronome/src/components/Landing/Landing.tsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import Branding from '../../partials/Branding/Branding'
+import UseItEverywhere from '../../partials/UseItEverywhere/UseItEverywhere'
 import HeaderMenu from '../../shared/partials/HeaderMenu/HeaderMenu'
-
+import "./Landing.scss"
 export default class Landing extends Component {
     render() {
         return (
             <>
                 <HeaderMenu />
-                <Branding />
+                <div className='landing-container'>
+                    <Branding />
+                    <UseItEverywhere />
+                </div>
             </>
         )
     }
