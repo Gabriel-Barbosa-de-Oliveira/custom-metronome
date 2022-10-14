32bbca7f156947761080c9021b665a6f915ea452
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2022-10-13 22:25:43 -0300

    Bind Values nos formularios

--- custom-metronome/src/partials/Login/Login.tsx
+++ custom-metronome/src/partials/Login/Login.tsx
@@ -1,13 +1,37 @@
 import FormControl from '@mui/material/FormControl'
 import TextField from '@mui/material/TextField'
 import React, { Component } from 'react'
+import { ILogin } from '../../shared/interfaces/states/ILogin'
 
-export default class Login extends Component {
+export default class Login extends Component<{}, ILogin> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleInputChange = (event: any, inputType: string) => {
+    const { value } = event.target;
+    const newState: any = {};
+    newState[inputType] = value;
+    this.setState(newState);
+  }
+
   render() {
+
+    let { email, password } = this.state;
+
     return (
       <FormControl fullWidth sx={{ display: "grid", gridTemplateColumns: "1fr", gridGap: 16 }} variant="standard">
-        <TextField id="email" label="E-mail" variant="standard" />
-        <TextField id="password" label="Senha" variant="standard" type={"password"} />
+        <TextField id="email" label="E-mail" variant="standard" value={email} onChange={(event) => {
+                    this.handleInputChange(event, "email");
+                }} />
+        <TextField id="password" label="Senha" variant="standard" type={"password"} value={password} onChange={(event) => {
+                    this.handleInputChange(event, "password");
+                }} />
       </FormControl>
     )
   }
